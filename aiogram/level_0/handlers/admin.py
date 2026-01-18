45cc193d0ad36cb221cdcd9979cfe85950f76425
trecer <75448852+trecer@users.noreply.github.com>
2026-01-18 20:12:20 +0300

    Добавил обработчики для админов

---
+++ aiogram/level_0/handlers/admin.py
@@ -0,0 +1,15 @@
+from aiogram import Router
+from aiogram.types import CallBackQuery
+from aiogram.fsm.context import FSMContext
+
+
+router = Router()
+
+
+@router.callback_query(F.data == "get_users")
+async def call_get_users(callback_query, state: FSMContext, users: list):
+  await callback_query.answer()
+  
+  text = "Анкеты пользователей:"
+  await callback_query.message.answer(text)
+  
