b9a227d96965cff7ebe96bea4ed8dee988b47636
trecer <75448852+trecer@users.noreply.github.com>
2026-01-18 19:18:29 +0300

    добавил файл запуска бота main.py

--- aiogram/level_0/main.py
+++ aiogram/level_0/main.py
@@ -1,1 +1,19 @@
+import asyncio
+
+from aiogram import Dispatcher, Router, Bot
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from config import settings
+
+
+async def main():
+    storage = MemoryStorage()
+    bot = Bot(token=settings.bot_token)
+    dp = Dispatcher(storage=storage)
+    
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
