2d6ac171c101091c7301ce9569b2f6c6fd3a88ec
trecer <75448852+trecer@users.noreply.github.com>
2026-01-19 04:16:12 +0300

    Добавил возможный функционал фильтрации

--- aiogram/level_0/handlers/admin.py
+++ aiogram/level_0/handlers/admin.py
@@ -6,6 +6,10 @@ from aiogram.fsm.context import FSMContext
 router = Router()
 
 
+# router.message.filter(F.from_user.id.in_({admin_ids})
+# router.callback_query.filter(F.from_user.id.in_({admin_ids})
+
+
 @router.callback_query(F.data == "get_users")
 async def call_get_users(callback_query, state: FSMContext, users: list):
   await callback_query.answer()
